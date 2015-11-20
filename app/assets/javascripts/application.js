cb317078de663f05de0adf6b80e509e05fc06c56
teckjon <demospata@hotmail.com>
2015-11-20 18:46:39 +0000

    Checkpoint 7, Java completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+  var blocmetrics = {};
+    blocmetrics.report = function(eventName){
+   // #1
+   var event = {event: { name: eventName }};
+ 
+   // #2
+   var request = new XMLHttpRequest();
+   // #3
+   request.open("POST", "http://localhost:3000/api/events", true);
+   // #4
+   request.setRequestHeader('Content-Type', 'application/json');
+   // #5
+   request.send(JSON.stringify(event));
+  };
