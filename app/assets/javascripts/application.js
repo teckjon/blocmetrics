4e9474e9bd4363f56bd33bcee0a95245c2fc70f1
teckjon <demospata@hotmail.com>
2015-11-23 19:01:04 +0000

    Application.js API

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,7 @@
    // #2
    var request = new XMLHttpRequest();
    // #3
-   request.open("POST", "http://localhost:3000/api/events", true);
+   request.open("POST", "https://blocmetrics-teckjon.c9users.io/api/events", true);
    // #4
    request.setRequestHeader('Content-Type', 'application/json');
    // #5
